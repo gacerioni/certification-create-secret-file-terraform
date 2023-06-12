aebfa225796e9fe163ae19a9ae6861431eb5cf0a
gacerioni <gacerioni@gmail.com>
2023-06-12 12:11:21 -0300

    enhancing the random mechanism

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
+resource "random_string" "random_secret" {
+  length = 16
+  special = false
+}
+
 resource "local_file" "super_secret_file" {
-  content  = "Behold the super secret: ${var.secret_word}"
+  content  = "Behold the super secret: ${random_string.random_secret.result}"
   filename = "/opt/certification/super_secret.txt"
 }
